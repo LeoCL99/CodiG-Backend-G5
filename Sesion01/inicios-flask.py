fb2bebec957c0883214b23b3469ead20696f2403
LeoCL99 <leo987444@hotmail.com>
2022-01-10 14:57:02 -0500

    chore: avance flask

--- Sesion01/inicios-flask.py
+++ Sesion01/inicios-flask.py
@@ -1,3 +1,4 @@
+from logging import debug
 from flask import Flask
 #en python tenemos variables que son de uso propio de pytho no podemos modificar ni alterar
 #__name__> es variable sirve para indicar si estamos en el archivo principal del proyecto
@@ -6,4 +7,15 @@ app=Flask(__name__)
 @app.route('/')
 def inicio():
     return 'Bienvenido a mi API'
-app.run()
+
+@app.route('/bienvenido')
+def bienvenido():
+    return 'Te doy la bienvenida a mi API'
+@app.route ('/bienvenido/home')
+def bienvenido_home():
+    # solamente podemos retornar los siguientes tipos de datos:String, tupla y diccionarios
+    return 'Te doy la bienvenida a mi API home'
+if __name__ == '__main__': 
+    # para que cada vez que nosotros hagamos algun cambio en cualquier archivo del proyecto y se  reinicie autmaticamente
+    # entonces deberemos indicar el parametro debug con el valor de True(cuyo valor por defecto es False)           
+ app.run(debug=True)
